d632462ce574c4a2cf5c26ebbb5d497cc0cefaa0
ratzz-zz <yael.lopez.103@gmail.com>
2025-09-11 16:57:42 -0600

    Initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h1 class="text-center mb-4">Calorie Counter</h1>
+        <div class="card">
+          <div class="card-body">
+            <p class="text-center">Welcome to your Calorie Counter app!</p>
+            <p class="text-muted text-center">Start tracking your daily calorie intake.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      // Component data will go here
+    }
+  }
+}
+</script>
+
+<style>
+/* Custom styles will go here */
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    alias: {
+      vue: '@vue/runtime-dom'
+    },
+    extensions: ['.js', '.vue', '.json'],
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+}
